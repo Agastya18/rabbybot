570c6a87022236feda543739786c01bf5686622c
agastya gaur <agastyagaur17@gmail.com>
2024-12-11 16:50:24 +0530

    add bot

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,9 @@ import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
 import { config } from './ config.ts'
+import WebApp from '@twa-dev/sdk'
 const queryClient = new QueryClient()
+WebApp.ready()  
 createRoot(document.getElementById('root')!).render(
   
    <WagmiProvider config={config}>
